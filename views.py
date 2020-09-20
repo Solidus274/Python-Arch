d616472bcc5c7708060d65335919fc801ea4f1f8
Solidus274 <solidus274@gmail.com>
2020-09-20 16:48:51 +0300

    Задания 1 и 2 выполнены

--- views.py
+++ views.py
@@ -10,3 +10,16 @@ def main_view(request):
 def about_view(request):
     # Просто возвращаем текст
     return '200 OK', "About"
+
+
+def contact_view(request):
+    # Проверка метода запроса
+    if request['method'] == 'POST':
+        data = request['data']
+        title = data['title']
+        text = data['text']
+        email = data['email']
+        print(f'Нам пришло сообщение от {email} с темой {title} и текстом {text}')
+        return '200 OK', render('contact.html')
+    else:
+        return '200 OK', render('contact.html')
